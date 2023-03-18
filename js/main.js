f0151c4592c9adb50efd84b090f715060745c5fa
Farfama <farfama.hargaaya@gmail.com>
2023-03-18 22:14:50 +0900

    Update moving rect with mouse

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ function preload() {
 //waits for what's in preload before executing (runs only once)
 function setup() {
     createCanvas(windowWidth,windowHeight);
-    rectMode(CENTER); // move the drawing origin of the rectangle to the center
     strokeWeight(5);
 
 }
@@ -21,13 +20,5 @@ function draw() {
 
     stroke('yellow');
     // noStroke();
-    translate(width/2, height/2); // move origin of sketch to the center
-    rect(0,0,500,500); // (0, 0) = location of upper left corner, (500) = width, (500) = height 
-
-    push()
-        fill(255,255,0) //this will only refers to the ellipse
-        ellipse(0,0,50)
-    pop()
-
-    rect(110,0, 500, 500) // this uses the fill after background()
+    rect(mouseX, mouseY,500,500); // (0, 0) = location of upper left corner, (500) = width, (500) = height 
 }

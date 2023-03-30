71fc38753a765e58a1d78f879a2b5cd5a9c0e31d
Farfama <farfama.hargaaya@gmail.com>
2023-03-30 14:40:29 +0900

    Update audio reactive rect

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //declare functions/variables here that are global
-let audio;
+let audio, amp;
 
 let posX = 0;
 let posY = 0;
@@ -12,12 +12,17 @@ function preload() {
 function setup() {
     createCanvas(windowWidth,windowHeight);
     rectMode(CENTER);
-    console.log(audio);
+    amp = new p5.Amplitude();
+    audio.play();
 
 }
 // runs continuous at 60fps
 function draw() {
     background(0) // set the background color of canvas
     translate(width/2, height / 2);
+    const volume = amp.getLevel();
+    const mapW = map(volume, 0, 0.1, 0, 500);
+    rect(0,0,mapW,mapW);
+
 
 }

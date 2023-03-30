60677ca4201e3d8b95be7a192bd7b13cc4c3bbdc
Farfama <farfama.hargaaya@gmail.com>
2023-03-30 14:30:17 +0900

    Add audio, p5sound

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 //declare functions/variables here that are global
+let audio;
 
 let posX = 0;
 let posY = 0;
 //helper function to pre-load assets (load sound here)
 function preload() {
+    audio = loadSound('audio/01.mp3')
 }
 
 //waits for what's in preload before executing (runs only once)
 function setup() {
     createCanvas(windowWidth,windowHeight);
-    strokeWeight(5);
     rectMode(CENTER);
+    console.log(audio);
 
 }
 // runs continuous at 60fps
@@ -18,29 +20,4 @@ function draw() {
     background(0) // set the background color of canvas
     translate(width/2, height / 2);
 
-    fill('red');
-    rect(posX,posY,500,500);
-}
-
-function keyPressed() {
-    switch(keyCode) {
-        case LEFT_ARROW:
-            posX -=10;
-            break;
-
-        case RIGHT_ARROW:
-            posX += 10;
-            break;
-
-        case UP_ARROW:
-            posY -= 10;
-            break;
-        
-        case DOWN_ARROW:
-            posY += 10;
-            break;
-        
-        default:
-            break;
-    }
 }

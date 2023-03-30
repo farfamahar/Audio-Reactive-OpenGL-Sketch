0be8f37cf36511f92cc18e90adc309757d0f91fc
Farfama <farfama.hargaaya@gmail.com>
2023-03-30 21:17:50 +0900

    Update loading animation

--- js/shader.js
+++ js/shader.js
@@ -2,18 +2,24 @@ let myShaders;
 let audio, amp, fft;
 let posX = 0;
 let posY = 0;
+let loading = true;
+let angle = 0;
 
 function preload() {
   myShaders = loadShader("shaders/vertex_noise.vert", "shaders/fragment.frag");
-  audio = loadSound("audio/01.mp3");
+  audio = loadSound("assets/01.mp3", loaded);
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   shader(myShaders);
-  // rectMode(CENTER);
+  //   rectMode(CENTER);
   amp = new p5.Amplitude();
   fft = new p5.FFT();
+}
+
+function loaded() {
+  loading = false;
   // Using the DOM library of p5.
   button = createButton("Play / Pause");
   button.mousePressed(toggleSound);
@@ -21,33 +27,42 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-  fft.analyze();
+  if (loading) {
+    background("black");
+    rotate(angle);
+    strokeWeight(4);
+    stroke(255);
+    line(0, 0, 50, 0);
+    angle += 0.2;
+  } else {
+    background(0);
+    fft.analyze();
 
-  let freq = fft.getCentroid();
-  const volume = amp.getLevel(); // 0 - 1
-  freq *= 0.001;
+    let freq = fft.getCentroid();
+    const volume = amp.getLevel(); // 0 - 1
+    freq *= 0.001;
 
-  const mapF = map(freq, 0, 1, 0, 20); // experiment with values
-  const mapA = map(volume, 0, 0.2, 0, 0.5); //experiment with values
+    const mapF = map(freq, 0, 1, 0, 20); // experiment with values
+    const mapA = map(volume, 0, 0.2, 0, 0.5); //experiment with values
 
-  myShaders.setUniform("uTime", frameCount);
-  myShaders.setUniform("uFrequency", mapF);
-  myShaders.setUniform("uAmp", mapA);
+    myShaders.setUniform("uTime", frameCount);
+    myShaders.setUniform("uFrequency", mapF);
+    myShaders.setUniform("uAmp", mapA);
 
-  sphere(width / 8, 200, 200);
+    sphere(width / 8, 200, 200);
 
-  stroke(255);
-  strokeWeight(0.5);
-  const waveform = fft.waveform();
+    stroke(255);
+    strokeWeight(0.5);
+    const waveform = fft.waveform();
 
-  beginShape();
-  for (let i = 0; i < waveform.length; i++) {
-    const x = map(i, 0, waveform.length, -width / 2, width / 2);
-    const y = map(waveform[i], -1, 1, -height / 2, height / 2);
-    point(x, y);
+    beginShape();
+    for (let i = 0; i < waveform.length; i++) {
+      const x = map(i, 0, waveform.length, -width / 2, width / 2);
+      const y = map(waveform[i], -1, 1, -height / 2, height / 2);
+      point(x, y);
+    }
+    endShape();
   }
-  endShape();
 }
 
 function windowResized() {

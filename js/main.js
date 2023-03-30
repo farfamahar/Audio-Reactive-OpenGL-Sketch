39bc49324eb3f0b7c1509dade68376652f92aa01
Farfama <farfama.hargaaya@gmail.com>
2023-03-30 14:50:41 +0900

    Update moving wayform

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //declare functions/variables here that are global
-let audio, amp;
+let audio, amp, fft;
 
 let posX = 0;
 let posY = 0;
@@ -14,21 +14,31 @@ function setup() {
     rectMode(CENTER);
     amp = new p5.Amplitude();
     audio.play();
+    fft = new p5.FFT();
 
 }
 // runs continuous at 60fps
 function draw() {
     background(0) // set the background color of canvas
     stroke(255);
-    translate(0, height / 2);
+    // translate(0, height / 2);
     const volume = amp.getLevel();
     // const mapW = map(volume, 0, 0.1, 0, 500);
     // rect(0,0,mapW,mapW);
 
-    const waveform = audio.getPeaks();
+    // const waveform = audio.getPeaks();
+
+    // for(let i = 0; i < waveform.length; i++){
+    //     line(i, waveform[i] * 100, i, waveform[i] * -10)
+    // }
+
+    const waveform = fft.waveform();
 
     for(let i = 0; i < waveform.length; i++){
-        line(i, waveform[i] * 100, i, waveform[i] * -10)
+        const x = map(i, 0, waveform.length, 0, width);
+        const y = map(waveform[i], -1, 1, 0, height);
+        point(x, y);
     }
+
 
 }
